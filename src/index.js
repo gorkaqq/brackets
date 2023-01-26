2ae4b26a6e981051b7d8af2b2834410bf12d62ef
gorkaqq <gorka1233@gmail.com>
2023-01-26 22:01:09 +0400

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+
+  let config = bracketsConfig.length;
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    if (currentSymbol == '|' || currentSymbol == '7' || currentSymbol == '8') {
+      if (stack[stack.length - 1] == currentSymbol) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      for (let j = 0; j < config; j++) {
+        if (bracketsConfig[j][0] === currentSymbol) {
+          stack.push(currentSymbol);
+        } else if (bracketsConfig[j][1] === currentSymbol) {
+          if (
+            stack.length !== 0 &&
+            bracketsConfig[j][0] === stack[stack.length - 1]
+          ) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return stack.length === 0;
+};
