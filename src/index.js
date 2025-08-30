4b091493eac2439f0cab40cfa66ac1fe9d15874f
gorkaqq <gorka1233@gmail.com>
2025-08-30 20:29:30 +0400

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const openBrackets = [];
+  const closeBrackets = {};
+  const sameBrackets = [];
+
+  bracketsConfig.forEach((config) => {
+    const [open, close] = config;
+    openBrackets.push(open);
+    closeBrackets[close] = open;
+    if (open === close) {
+      sameBrackets.push(open);
+    }
+  });
+
+  for (let i = 0; i < str.length; i += 1) {
+    const currentSymbol = str[i];
+
+    if (openBrackets.includes(currentSymbol)) {
+      if (sameBrackets.includes(currentSymbol)) {
+        const topElement = stack[stack.length - 1];
+
+        if (stack.length && topElement === currentSymbol) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      const topElement = stack[stack.length - 1];
+
+      if (closeBrackets[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
